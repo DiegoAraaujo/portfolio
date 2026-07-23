c80989680900cd55ea8d99f40bded7ab39374082
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-07-23 20:13:34 -0300

    feat: configure app layout and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { Header } from './components/Header'
+import About from './sections/about'
+import TimeLine from './sections/timeline'
+import Skills from './sections/Skills'
+import Projects from './sections/Projects'
+import Certifications from './sections/Certifications'
+import Contacts from './sections/contacts'
+import TimelineDetail from './sections/timeline/components/TimelineDetail'
+import ProjectDetail from './sections/Projects/components/ProjectDetail'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
+
 const App = () => {
-  return <></>
+  return (
+    <div className="page-bg">
+      <BrowserRouter>
+        <ScrollToTop />
+
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <About />
+                <TimeLine />
+                <Skills />
+                <Projects />
+                <Certifications />
+                <Contacts />
+              </>
+            }
+          />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/timeline/:id" element={<TimelineDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
